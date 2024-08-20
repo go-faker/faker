9c8f8861697817b566c3a80f2730cc8247ab127f
Marin Bezhanov <marin.bezhanov@gmail.com>
2024-08-20 16:23:29 +0300

    feat: add UserAgent faker

---
+++ user_agent_test.go
@@ -0,0 +1,31 @@
+package faker
+
+import (
+	"reflect"
+	"strings"
+	"testing"
+)
+
+func TestUserAgent(t *testing.T) {
+	layoutEngines := []string{"Gecko", "KHTML", "Presto", "Trident"}
+	uaStringContains := func(elems []string, v string) bool {
+		for _, s := range elems {
+			if strings.Contains(v, s) {
+				return true
+			}
+		}
+		return false
+	}
+
+	ua, err := GetUserAgent().UserAgent(reflect.Value{})
+
+	if err != nil {
+		t.Error("Expected not error, got err", err)
+	}
+	if strings.Index(ua.(string), "Mozilla/5.0") != 0 {
+		t.Error("Expected User Agent string to begin with 'Mozilla/5.0'")
+	}
+	if !uaStringContains(layoutEngines, ua.(string)) {
+		t.Error("Expected User Agent string to contain a valid layout engine")
+	}
+}
