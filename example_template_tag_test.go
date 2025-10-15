880a511542b313beb6bd2b9d6fc6e02413cb67d3
zes <rizesky.dev@gmail.com>
2025-10-15 18:08:39 +0700

    feat: add template tag to enable derived values

---
+++ example_template_tag_test.go
@@ -0,0 +1,34 @@
+package faker_test
+
+import (
+	"fmt"
+
+	"github.com/go-faker/faker/v4"
+)
+
+type Address struct {
+	Street string `faker:"street_name"`
+	City   string `faker:"city"`
+}
+
+type UserProfile struct {
+	FirstName string `faker:"first_name"`
+	LastName  string `faker:"last_name"`
+	Domain    string `faker:"domain_name"`
+
+	// Template uses helper 'lower' and 'slug' (slug is lower+dash)
+	Username string `faker:"template:{{.FirstName | lower}}.{{.LastName | lower}}"`
+	Slug     string `faker:"template:{{.FirstName | slug}}-{{.LastName | slug}}"`
+	Email    string `faker:"template:{{.Username}}@{{.Domain}}"`
+	// (only template-related fields kept for this example)
+}
+
+func Example_templateTag() {
+	var v UserProfile
+	if err := faker.FakeData(&v); err != nil {
+		fmt.Println("error:", err)
+		return
+	}
+	// output the filled struct; values are random so we don't assert exact output
+	fmt.Printf("%+v\n", v)
+}
